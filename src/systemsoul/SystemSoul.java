2ff853bf4fec18bd98dbb4fb93d28a55b7722ab7
Bbenex <bbenikeszi@gmail.com>
2015-03-22 20:09:06 +0100

    commit

--- src/systemsoul/SystemSoul.java
+++ src/systemsoul/SystemSoul.java
@@ -4,7 +4,7 @@ public class SystemSoul {
 
     
     public static void main(String[] args) {
-   new FaceGUI().setVisible(true);
+        new FaceGUI().setVisible(true);
         //new Login().setVisible(true);
         // TODO code application logic here
     }
